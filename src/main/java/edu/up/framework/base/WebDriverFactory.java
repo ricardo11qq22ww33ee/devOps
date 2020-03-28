07fd99d9d67f4b9f72dc8395e02e6adbe57cdafd
Oscar Garcia <ogcdevelopment@outlook.com>
2020-03-28 10:20:10 -0500

    Cleaning branch

--- src/main/java/edu/up/framework/base/WebDriverFactory.java
+++ src/main/java/edu/up/framework/base/WebDriverFactory.java
@@ -13,32 +13,5 @@ import java.util.Random;
 
 public class WebDriverFactory {
     private final static Logger logger = LoggerFactory.getLogger(WebDriverFactory.class);
-    private static int iPosX;
-    private static int iPosY;
 
-    static WebDriver createInstance(){
-        WebDriver driver;
-        DesiredCapabilities desiredCapabilities;
-        System.setProperty("webdriver.chrome.driver","./driver/chromedriver.exe");
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("test-type");
-        options.addArguments("disable-popup-blocking");
-        options.addArguments("disable-extensions");
-        desiredCapabilities = DesiredCapabilities.chrome();
-        desiredCapabilities.setJavascriptEnabled(true);
-        desiredCapabilities.setBrowserName("chrome");
-        driver = new ChromeDriver(options);
-
-        Random rn = new Random();
-        iPosX = rn.nextInt(600 - 50 + 1) + 50;
-        iPosY = rn.nextInt(100 - 10 + 1) + 40;
-        driver.manage().window().setPosition(new Point(iPosX, iPosY));
-        driver.manage().window().setSize(new Dimension(1000, 800));
-        logger.info("Thread id = " + Thread.currentThread().getId());
-        logger.info("Hashcode failure Web Driver: " + driver.hashCode());
-        logger.info("Browser selected: "+desiredCapabilities.getBrowserName());
-
-
-        return  driver;
-    }
 }
